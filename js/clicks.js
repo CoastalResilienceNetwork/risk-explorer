0d12bc2dd6936417a8c46cc38fe5caf5fe8ca270
Casey Schneebeck <cschneebeck@tnc.org>
2018-03-01 13:30:25 -0700

    Create HTML for app

---
+++ js/clicks.js
@@ -0,0 +1,16 @@
+define([
+	"dojo/_base/declare", "esri/tasks/query", "esri/tasks/QueryTask", "esri/graphicsUtils"
+],
+function ( declare, Query, QueryTask, graphicsUtils ) {
+        "use strict";
+
+        return declare(null, {
+			eventListeners: function(t){
+						 	
+			},
+			makeVariables: function(t){
+				
+			}
+        });
+    }
+);
---
+++ js/esriapi.js
@@ -0,0 +1,34 @@
+define([
+	"esri/layers/ArcGISDynamicMapServiceLayer", "esri/geometry/Extent", "esri/SpatialReference", "esri/tasks/query" ,"esri/tasks/QueryTask", "dojo/_base/declare", "esri/layers/FeatureLayer", 
+	"esri/symbols/SimpleLineSymbol", "esri/symbols/SimpleFillSymbol","esri/symbols/SimpleMarkerSymbol", "esri/graphic", "dojo/_base/Color", "dojo/_base/lang",
+	"esri/tasks/IdentifyTask", "esri/tasks/IdentifyParameters",
+],
+function ( 	ArcGISDynamicMapServiceLayer, Extent, SpatialReference, Query, QueryTask, declare, FeatureLayer, 
+			SimpleLineSymbol, SimpleFillSymbol, SimpleMarkerSymbol, Graphic, Color, lang,
+			IdentifyTask, IdentifyParameters) {
+        "use strict";
+
+        return declare(null, {
+			esriApiFunctions: function(t){	
+				// Add dynamic map service
+				t.dynamicLayer = new ArcGISDynamicMapServiceLayer(t.url, {opacity:0.7});
+				t.map.addLayer(t.dynamicLayer);
+				t.dynamicLayer.setVisibleLayers(t.obj.visibleLayers);
+			
+				t.dynamicLayer.on("load", function () { 			
+					t.layersArray = t.dynamicLayer.layerInfos;
+					// Save and Share Handler					
+					if (t.obj.stateSet == "yes"){
+						//extent
+						var extent = new Extent(t.obj.extent.xmin, t.obj.extent.ymin, t.obj.extent.xmax, t.obj.extent.ymax, new SpatialReference({ wkid:4326 }))
+						t.map.setExtent(extent, true);
+						t.obj.stateSet = "no";
+					}	
+				});		
+			},
+			clearAtts: function(t){
+				t.map.graphics.clear();
+			} 				
+		});
+    }
+);
